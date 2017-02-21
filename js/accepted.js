a5f165d2af666c21b60de82654f5a4cbe28af353
Kay McKelly <frecklestars@gmail.com>
2017-02-21 15:42:25 -0800

    Code tidying, adding Handlebars to acceptedpapers.html

---
+++ js/accepted.js
@@ -0,0 +1,6 @@
+$.getJSON('./json/papers.json', function(data) {
+    var theTemplateScript = $("#acceptedScript").html();
+    var theTemplate = Handlebars.compile(theTemplateScript);
+    var theCompiledHtml = theTemplate(data);
+    $('#accepted').html(theCompiledHtml);
+});
