3e9ad6fd900987e0f88551b4e68a0cc92fadc8d6
Kay McKelly <frecklestars@gmail.com>
2017-03-27 14:24:20 -0700

    Getting dates from JSON, updating countdown timers so all are now functional

--- www/js/cryptoCountdowns.js
+++ www/js/cryptoCountdowns.js
@@ -1,79 +1,86 @@
-// commented out all code not currently in use
+// getting dates from JSON
+$.getJSON('./json/metadata.json', function(data) {
+  $('.submissionDate').text(data.submitdate);
+  $('.firstRoundNotif').text(data.firstRound);
+  $('.rebuttal').text(data.rebuttalDue);
+  $('.finalNotif').text(data.finalNotification);
+  $('.cameraReady').text(data.cameraReady);
+})
+.fail(function(jqxhr, textStatus, error) {
+  console.log('There\'s an issue with metadata.json. The problem is ' + error);
+});
 
-//countdown for submission deadline
-// $('#submission').countdown('2017/02/08 02:00')
-// .on('update.countdown', function(event) {
-//   var format = ' in %D days %H:%M';
-//   if(event.offset.totalDays === 1) {
-//     format = ' in %-d day%!d %H:%M';
-//   }
-//   $(this).html(event.strftime(format));
-// })
-// .on('finish.countdown', function(event) {
-//   $(this).html(' has passed');
-// });
-
-
-//countdown for notification of decision
-// $('#notDecis').countdown('2017/05/08')
-// .on('update.countdown', function(event) {
-//   var format = ' in %D days';
-//   if(event.offset.totalDays === 1) {
-//     format = ' in %-d day%!d';
-//   }
-//   $(this).html(event.strftime(format));
-// })
-// .on('finish.countdown', function(event) {
-//   $(this).html('This deadline has passed ');
-// });
+// countdown for submission deadline
+$('#submission').countdown('2017/05/19 02:00')
+.on('update.countdown', function(event) {
+  var format = ' in %D days %H:%M';
+  if(event.offset.totalDays === 1) {
+    format = ' in %-d day%!d %H:%M';
+  }
+  $(this).html(event.strftime(format));
+})
+.on('finish.countdown', function(event) {
+  $(this).html(' has passed');
+});
 
 
-//countdown for proceedings version
-// $('#procVer').countdown('2017/06/05')
-// .on('update.countdown', function(event) {
-//   var format = ' due in %D days';
-//   if(event.offset.totalDays === 1) {
-//     format = ' due in %-d day%!d';
-//   }
-//   $(this).html(event.strftime(format));
-// })
-// .on('finish.countdown', function(event) {
-//   $(this).html(' has occurred');
-// });
+//countdown for first round of notifications
+$('#1stRound').countdown('2017/07/09')
+.on('update.countdown', function(event) {
+  var format = ' in %D days';
+  if(event.offset.totalDays === 1) {
+    format = ' in %-d day%!d';
+  }
+  $(this).html(event.strftime(format));
+})
+.on('finish.countdown', function(event) {
+  $(this).html('This deadline has passed ');
+});
 
-// NOTE: there is currently no date for this in the html; don't forget to edit this to match the others
-//countdown for registration date
-//countdown for early registration
-// $('#earlyReg').countdown('2017/07/31')
-// .on('update.countdown', function(event) {
-//   var format = '%D days left ';
-//   if(event.offset.totalDays === 1) {
-//     format = '%-d day%!d left';
-//   }
-//   $(this).html(event.strftime(format));
-// })
-// .on('finish.countdown', function(event) {
-//   $(this).html('This deadline has passed ');
-// });
 
-// NOTE: there is currently no date for this in the html; don't forget to edit this to match the others
-//countdown for registration date
-// $('#reg').countdown('2017/08/10 07:00')
-// .on('update.countdown', function(event) {
-//   var format = '%D days %H:%M left ';
-//   if(event.offset.totalDays === 1) {
-//     format = '%-d day%!d %H:%M left';
-//   }
-//   $(this).html(event.strftime(format));
-// })
-// .on('finish.countdown', function(event) {
-//   $(this).html('This date has passed ');
-// });
+//countdown for rebuttals due
+$('#rebuttals').countdown('2017/07/14')
+.on('update.countdown', function(event) {
+  var format = ' due in %D days';
+  if(event.offset.totalDays === 1) {
+    format = ' due in %-d day%!d';
+  }
+  $(this).html(event.strftime(format));
+})
+.on('finish.countdown', function(event) {
+  $(this).html(' has occurred');
+});
+
+// countdown for final notification
+$('#finalNotification').countdown('2017/08/13')
+.on('update.countdown', function(event) {
+  var format = ' in %D days';
+  if(event.offset.totalDays === 1) {
+    format = ' due in %-d day%!d';
+  }
+  $(this).html(event.strftime(format));
+})
+.on('finish.countdown', function(event) {
+  $(this).html(' has occurred');
+});
+
+// countdown for cameraReady deadline
+$('#camera').countdown('2017/09/07')
+.on('update.countdown', function(event) {
+  var format = ' due in %D days';
+  if(event.offset.totalDays === 1) {
+    format = ' due in %-d day%!d';
+  }
+  $(this).html(event.strftime(format));
+})
+.on('finish.countdown', function(event) {
+  $(this).html(' has occurred');
+});
 
 // countdown for conference date
 $('#conf').countdown('2017/12/03')
 .on('update.countdown', function(event) {
-  var format = ' starts in %D days';
+  var format = ' begins in %D days';
   if(event.offset.totalDays === 1) {
     format = ' starts in %-d day%!d';
   }
