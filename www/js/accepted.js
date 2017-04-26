cae3f7fd2488acb9dc35ea03796ac4fe78241d27
Kay McKelly <frecklestars@gmail.com>
2017-04-26 08:33:01 -0700

    Proper nesting of divs and bootstrap class usage in program.html

--- www/js/accepted.js
+++ www/js/accepted.js
@@ -7,5 +7,8 @@ $(document).ready(function() {
   })
   .fail(function(jqxhr, textStatus, error) {
     console.log('There is a problem with metadata.json. The problem is ' + error);
+    
+    document.getElementById('renderedProgram');
+    renderedProgram.innerHTML = '<p class="alert alert-success" role="alert"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&ensp;The program is not currently available.</p>';
   });
 })
