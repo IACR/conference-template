23b29ae55e56d3d6aaa0b0e20ca4560ef43d2d77
Kay McKelly <frecklestars@gmail.com>
2017-05-18 17:15:50 -0700

    Improvements in error handling for acceptedpapers.html

--- www/js/accepted.js
+++ www/js/accepted.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
   .fail(function(jqxhr, textStatus, error) {
     if (textStatus === 'error') {
       console.log('papers.json not found, check file name and try again');
-      document.getElementById('accepted');
-      accepted.innerHTML = '<p class="alert alert-success" role="alert"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&ensp;The file that contains the list of accepted papers does not exist. Please check back later.</p>';
+      document.getElementById('errorBox');
+      errorBox.innerHTML = '<p>The list of accepted papers is not currently available. Please check back later.</p>';
     }
     else {
       console.log('There is a problem with papers.json. The problem is ' + error);
-      document.getElementById('accepted');
-      accepted.innerHTML = '<p class="alert alert-success" role="alert"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>&ensp;The list of accepted papers is not currently available. Please check back later.</p>';
+      document.getElementById('errorBox');
+      errorBox.innerHTML = '<p>The list of accepted papers is not currently available. Please check back later.</p>';
     }
   });
 })
