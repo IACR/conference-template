9d7151ed51081ab1903a24c98fb5148a61e5cdb3
Kay McKelly <frecklestars@gmail.com>
2018-10-09 11:12:14 -0700

    Update menus.js to use ajax instead of getjson

--- www/js/menus.js
+++ www/js/menus.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
   function getInfo() {
-    $.getJSON('./json/metadata.json', function(data) {
-      document.title = data.shortName + ' ' + document.title;
-      $('.conf_name').text(data.shortName);
-      $('.long_conf').text(data.name);
-      $('.conf_dates').text(data.dates);
-      $('.conf_location').text(data.location);
-      $('.conf_start').text(data.startdate);
-    })
-    .fail(function(jqxhr, textStatus, error) {
-     alert('There is a problem with metadata.json. The problem is ' + error);
-  });
+    $.ajax({
+      cache: false,
+      url: './json/metadata.json',
+      dataType: 'json',
+      success: function(data) {
+        document.title = data.shortName + ' ' + document.title;
+        $('.conf_name').text(data.shortName);
+        $('.long_conf').text(data.name);
+        $('.conf_dates').text(data.dates);
+        $('.conf_location').text(data.location);
+        $('.conf_start').text(data.startdate);
+      },
+      fail: function(jqxhr, textStatus, error) {
+       alert('There is a problem with metadata.json. The problem is ' + error);
+      }
+    });
 
     // fetch a fragment - the left nav - and insert it into the DOM
     $.get('./fragments/nav.html', function(data) {
