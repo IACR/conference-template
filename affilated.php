340f78d6c38257e5d0f1c02a6d39f98176d6ba83
Kay McKelly <frecklestars@gmail.com>
2019-12-10 08:43:12 -0800

    Update CORRECT nav file, add affiliated events and invited talks pages (just skeletons atm)

---
+++ affilated.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php // The header includes the head tag and start of body
+        require "includes/head.php";
+          ?>
+  </head>
+  <body>
+    <?php require "includes/nav.php"; ?>
+
+    <main class="container mt-5">
+
+      <!-- NOTE: below is standard placeholder text for when the page is under construction. please delete this entire <div class="row"> when ready to add other content -->
+      <div class="row">
+        <div class="col-sm-12">
+          <p>
+            This information is not yet available. Thank you for your patience.
+          </p>
+        </div>
+      </div>
+
+    </main>
+
+  <?php include "includes/footer.php"; ?>
+  </body>
+</html>
