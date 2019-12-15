ff7e5feb462294a09189e63406a58fc0db4c2e18
Kevin McCurley <github@digicrime.com>
2019-12-14 18:05:07 -0800

    Start on documentation.

--- includes/nav.php
+++ includes/nav.php
@@ -1,17 +1,24 @@
 <?php
+  // Set the root path in the nav menu.
   function rootPath() {
     // must declare $META as global variable, as it's created in head.php
     // and doesn't work here unless defined within the function
     global $META;
 
-    // NOTE: for debug purposes: if year is not an int, it'll fail to generate
-    // the correct paths in the nav. in metadata.json, you need to change year
-    // to an integer for production
-    if (empty($META['year'])) {
-        return "/";
-    } else {
+    // NOTE: if you are developing on a local machine, then you probably
+    // should use "/" as your root path. On the iacr.org server the URL is
+    // prefixed by the year (e.g., crypto.iacr.org/2020). We distinguish these
+    // cases using the $_SERVER['HTTP_HOST'] variable.
+
+    if (empty($_SERVER['HTTP_HOST'])) {
+      return "/";
+    }
+    $length = strlen($_SERVER['HTTP_HOST']);
+    if ($length >= 8 &&
+        substr($_SERVER['HTTP_HOST'], -8) === 'iacr.org') {
       return "/" . strval($META['year']) . "/";
     }
+    return "/";
   }
 ?>
 
