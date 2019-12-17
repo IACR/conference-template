14b3b2950ef5bc68449541953c0f71908835ccc2
Kevin McCurley <github@digicrime.com>
2019-12-17 23:02:57 +0000

    javascript version.

--- acceptedpapers.php
+++ acceptedpapers.php
@@ -17,60 +17,79 @@
       Accepted Papers
     </h2>
 
-    <!-- NOTE: if json/papers.json exists, then this will generate the list of papers. -->
-    <?php
-       if (file_exists('json/papers.json')) {
-         try {
-           $papers = json_decode(file_get_contents("json/papers.json"), true);
-           if ($papers === null) {
-             throw new Exception('json file is malformed');
-           }
-           $papers = $papers['acceptedPapers'];
-           echo '<p>In order of submission:</p>';
-           echo  '<ol id="accepted">';
-           foreach($papers as $paper) {
-             echo '<li><h4 class="paperTitle">' . $paper['title'] . '</h4>';
-	     echo ' <p>' . $paper['authors'] . '<br/>';
-             echo '  <span class="font-italic"> ' . $paper['affiliations'] . '</span></p></li>';
-           }
-           echo '</ol>';
-         } catch (Exception $e) {
-           echo '<p class="text-danger">papers.json file is malformed.</p>';
-         }
-       } else {
-         echo '<p>This information is not yet available. This information will be available after authors are notified, which should occur by ' . $META['finalNotification'] . '. Thank you for your patience.</p>';
-       }
-    ?>
-
-    <div class="row">
-      <section class="col-sm-12">
-        <ol id="accepted">
-
-          <!-- Handlebars import of accepted papers in websubrev export format -->
-          <script id="acceptedScript" type="text/x-handlebars-template">
-            {{#each acceptedPapers}}
-            <li>
-              <h4 class="paperTitle">
-                {{title}}
-              </h4>
-              <p>
-                {{authors}}
-                <br />
-                {{affiliations}}
-              </p>
-            </li>
-            {{/each}}
-          </script>
-
-        </ol>
-      </section>
+    <!-- NOTE: if json/papers.json exists, then this will be hidden. -->
+    <noscript>
+      <p>
+        <h4>This page requires javascript</h4>
+      </p>
+    </noscript>
+    <p id="notYetAvailable">
+      This information is not yet available. This information will be available after authors are notified, which should
+      occur by <?php echo $META['finalNotification'];?>. Thank you for your patience.
+    </p>
+    <!-- NOTE: if json/papers.json is malformed, then this will be shown. -->
+    <p id="jsonWarning" class="text-danger d-none">
+      The papers.json file is malformed.
+    </p>
+    <!-- NOTE: if json/papers.json is fetched and parsed, then this is populated. -->
+    <div id="accepted">
     </div>
-
+    <!-- Handlebars import of accepted papers in websubrev export format. See
+         json/sample_papers.json  -->
+    <script id="acceptedScript" type="text/x-handlebars-template">
+      <p>
+        These papers are listed in order of submission.
+      </p>
+      <ol id="accepted">
+        {{#each acceptedPapers}}
+        <li>
+          <h4 class="paperTitle">
+            {{title}}
+          </h4>
+          <p>
+            {{authors}}
+            <br />
+            {{affiliations}}
+          </p>
+        </li>
+        {{/each}}
+      </ol>
+    </script>
   </main>
 
   <?php include "includes/footer.php";?>
     <!-- Handlebars -->
     <script src="https://iacr.org/libs/js/handlebars/handlebars-v4.1.0.js" type="text/javascript"></script>
-
+    <script>
+     try {
+       fetch('json/papers.json',
+             {credentials: 'same-origin'})
+         .then(response => {
+           console.dir(response);
+           console.dir(response.status);
+           if (response.ok && response.status == 200) {
+             return response.json();
+           }
+         })
+         .then(data => {
+           if (data) {
+             var theTemplateScript = $("#acceptedScript").html();
+             var theTemplate = Handlebars.compile(theTemplateScript);
+             var theCompiledHtml = theTemplate(data);
+             $('#accepted').html(theCompiledHtml);
+             $('#notYetAvailable').hide();
+           }
+         })
+         .catch((e) => {
+           console.log('an error occurred');
+           $('#jsonWarning').removeClass('d-none');
+           console.dir(e);
+         });
+     } catch (error) {
+       console.dir(error);
+       $('jsonWarning').html('The server is failing or your browser is not supported.');
+       $('#jsonWarning').removeClass('d-none');
+     }
+    </script>
 </body>
 </html>
