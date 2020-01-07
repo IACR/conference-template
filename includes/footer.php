a272104ab0c7ea8360ae9f691c12c0f306b8de36
Kevin McCurley <github@digicrime.com>
2020-01-07 09:24:45 -0800

    This footer is like www.iacr.org

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,12 @@
-    <div class="row">
-      <footer class="col-12 text-center footer">
-        <small>
-          <span class="conf_name"><?php echo $META['name'];?></span>
-        </small>
-      </footer>
-    </div>
+    <footer class="text-center footer">
+      <span class="align-middle">
+        <span class="conf_name"><?php echo $META['shortName'];?></span><br/>
+        <a href="https://iacr.org/copyright.html">Copyright &copy;
+          <?php echo date("Y");?></a>
+        <span class="d-none d-md-inline">by the </span><span class="d-md-none">IACR</span>
+        <span class="d-none d-md-inline">International Association for Cryptologic Research</span></a>
+        <span class="d-none d-md-inline">&bull; </span><br class="d-md-none"> <a href="https://iacr.org/privacy.html">Privacy Policy</a></span>
+    </footer>
 
     <script src="https://iacr.org/libs/js/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://iacr.org/libs/css/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
