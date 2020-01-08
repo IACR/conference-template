1998e718855ee08a19eeada247b73264e8a9c4b6
Kevin McCurley <github@digicrime.com>
2020-01-08 09:46:08 -0800

    Remove extraneous span.

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,11 @@
     <footer class="text-center footer">
       <small>
-        <span class="align-middle">
-          <span class="conf_name"><?php echo $META['shortName'];?></span><br/>
-          <a href="https://iacr.org/copyright.html">Copyright &copy;
-            <?php echo date("Y");?></a>
-          <span class="d-none d-md-inline">by the </span><span class="d-md-none">IACR</span>
-          <span class="d-none d-md-inline">International Association for Cryptologic Research</span></a>
-          <span class="d-none d-md-inline">&bull; </span><br class="d-md-none"> <a href="https://iacr.org/privacy.html">Privacy Policy</a></span>
+        <span class="conf_name"><?php echo $META['shortName'];?></span><br/>
+        <a href="https://iacr.org/copyright.html">Copyright &copy;
+          <?php echo date("Y");?></a>
+        <span class="d-none d-md-inline">by the </span><span class="d-md-none">IACR</span>
+        <span class="d-none d-md-inline">International Association for Cryptologic Research</span></a>
+        <span class="d-none d-md-inline">&bull; </span><br class="d-md-none"> <a href="https://iacr.org/privacy.html">Privacy Policy</a></span>
       </small>
     </footer>
 
