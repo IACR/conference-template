991828f598093ff2d7304545393394aa736e3dd3
Kay McKelly <frecklestars@gmail.com>
2020-01-14 15:47:48 -0800

    Improve sponsors page, check in past sponsor logos

--- sponsors.php
+++ sponsors.php
@@ -23,7 +23,7 @@
       <div class="row">
         <div class="col-12">
           <p>
-            This information is not yet available. Thank you for your patience. If it were available, this is what this page might look like.
+            This information is not yet available. Thank you for your patience.
           </p>
         </div>
       </div>
@@ -41,146 +41,104 @@
         Platinum Level
       </h3>
 
-      <section class="row">
-        <div class="col-12 col-md-10 col-lg-6 mx-auto">
+      <div class="d-flex flex-wrap align-items-center">
+        <div class="sponsorLogoBox justify-content-between">
           <a href="http://kaymckelly.com">
-            <img src="./images/demo/logo1.jpg" alt="fake company 1" class="sponsorPageLogo">
+            <img src="./images/sponsors/ethereumFoundation.png" alt="Ethereum Foundation" title="Ethereum Foundation" class="sponsorPageLogo">
           </a>
-          <div class="caption">
-            <p class="lead text-center">
-              Scribble Circle Inc
-            </p>
-            <p>
-              This company is not a real sponsor, nor is it a real company.
-            </p>
-          </div>
         </div>
-        <div class="col-12 col-md-10 col-lg-6 mx-auto">
+        <div class="sponsorLogoBox justify-content-between">
           <a href="http://kaymckelly.com">
-            <img src="./images/demo/logo2.jpg" alt="fake company 1" class="sponsorPageLogo">
+            <img src="./images/sponsors/AWS.png" alt="AWS" title="AWS" class="sponsorPageLogo">
           </a>
-          <div class="caption">
-            <p class="lead text-center">
-              The Shouty Corporation
-            </p>
-            <p>
-              This company is not a real sponsor, nor is it a real company. But if it were, I'd probably register this name because that would be pretty funny. Perhaps <a href="http://kaymckelly.com">my main domain</a> is in for a name change?
-            </p>
-          </div>
         </div>
-      </section>
+      </div>
 
       <hr />
-
       <h3 class="pageSubtitle mt-4">
         Gold Level
       </h3>
 
-      <section class="row">
-        <div class="col-10 col-md-6 col-lg-4 mx-auto">
+      <div class="d-flex flex-wrap align-items-center">
+        <div class="sponsorLogoBox justify-content-between">
           <a href="http://kaymckelly.com">
-            <img src="./images/demo/logo5.jpg" alt="fake company 2" class="sponsorPageLogo">
+            <img src="./images/sponsors/cloudFlare.png" alt="Cloudflare" title="Cloudflare" class="sponsorPageLogo">
           </a>
-          <div class="caption">
-            <p class="lead text-center">
-              Seattle Umbrella Co
-            </p>
-            <p>
-              This company is not a real sponsor, nor is it a real company. Though it could be, given how much it rains in Seattle.
-            </p>
-          </div>
         </div>
-        <div class="col-10 col-md-6 col-lg-4 mx-auto">
+        <div class="sponsorLogoBox justify-content-between">
           <a href="http://kaymckelly.com">
-            <img src="./images/demo/logo4.jpg" alt="fake company 2" class="sponsorPageLogo">
+            <img src="./images/sponsors/inpher.png" alt="Inpher" title="Inpher" class="sponsorPageLogo">
           </a>
-          <div class="caption">
-            <p class="lead text-center">
-              Fast Talkers Ltd.
-            </p>
-            <p>
-              This company is not a real sponsor, nor is it a real company.
-            </p>
-          </div>
         </div>
-        <div class="col-10 col-md-6 col-lg-4 mx-auto">
+        <div class="sponsorLogoBox justify-content-between">
           <a href="http://kaymckelly.com">
-            <img src="./images/demo/logo5.jpg" alt="fake company 2" class="sponsorPageLogo">
+            <img src="./images/sponsors/google.png" alt="Google" title="Google" class="sponsorPageLogo">
           </a>
-          <div class="caption">
-            <p class="lead text-center">
-              Fake Umbrellas for Tourists
-            </p>
-            <p>
-              This company is not a real sponsor, nor is it a real company. And in case you were wondering what happens if a company's little blurb is a bit longer, this is what that looks like.
-            </p>
-            <p>
-              Additionally, this is what would happen if the blurb were split into two blocks of text, for whatever reason.
-            </p>
-          </div>
         </div>
-      </section>
+        <div class="sponsorLogoBox justify-content-between">
+          <a href="http://kaymckelly.com">
+            <img src="./images/sponsors/jPMorgan.jpg" alt="JPMorgan" title="JPMorgan" class="sponsorPageLogo">
+          </a>
+        </div>
+        <div class="sponsorLogoBox justify-content-between">
+          <a href="http://kaymckelly.com">
+            <img src="./images/sponsors/nsf.gif" alt="National Science Foundation" title="National Science Foundation" class="sponsorPageLogo">
+          </a>
+        </div>
+      </div>
 
       <hr />
-
       <h3 class="pageSubtitle mt-4">
         Silver Level
       </h3>
 
-      <section class="row">
-        <div class="col-6 col-md-4 col-lg-3 mx-auto">
+      <div class="d-flex flex-wrap align-items-center">
+        <div class="sponsorLogoBox justify-content-between">
           <a href="http://kaymckelly.com">
-            <img src="./images/demo/logo6.jpg" alt="fake company 2" class="sponsorPageLogo">
+            <img src="./images/sponsors/nucypher.png" alt="NuCypher" title="NuCypher" class="sponsorPageLogo">
           </a>
-          <div class="caption">
-            <p class="lead text-center">
-              Offset Circles
-            </p>
-            <p>
-              This company is not a real sponsor, nor is it a real company.
-            </p>
-          </div>
         </div>
-        <div class="col-6 col-md-4 col-lg-3 mx-auto">
+        <div class="sponsorLogoBox justify-content-between">
           <a href="http://kaymckelly.com">
-            <img src="./images/demo/logo7.jpg" alt="fake company 2" class="sponsorPageLogo">
+            <img src="./images/sponsors/QEDit.png" alt="QED-it" title="QED-it" class="sponsorPageLogo">
           </a>
-          <div class="caption">
-            <p class="lead text-center">
-              Waves Inc
-            </p>
-            <p>
-              This company is not a real sponsor, nor is it a real company. And again, if blurb length differs, this is testing what that might look like.
-            </p>
-          </div>
         </div>
-        <div class="col-6 col-md-4 col-lg-3 mx-auto">
+        <div class="sponsorLogoBox justify-content-between">
           <a href="http://kaymckelly.com">
-            <img src="./images/demo/logo6.jpg" alt="fake company 2" class="sponsorPageLogo">
+            <img src="./images/sponsors/mozilla.png" alt="Mozilla" title="Mozilla" class="sponsorPageLogo">
           </a>
-          <div class="caption">
-            <p class="lead text-center">
-              Offset Circles
-            </p>
-            <p>
-              This company is not a real sponsor, nor is it a real company.
-            </p>
-          </div>
         </div>
-        <div class="col-6 col-md-4 col-lg-3 mx-auto">
+        <div class="sponsorLogoBox justify-content-between">
           <a href="http://kaymckelly.com">
-            <img src="./images/demo/logo7.jpg" alt="fake company 2" class="sponsorPageLogo">
+            <img src="./images/sponsors/iotex.png" alt="IoTeX" title="IoTeX" class="sponsorPageLogo">
           </a>
-          <div class="caption">
-            <p class="lead text-center">
-              Waves Inc
-            </p>
-            <p>
-              This company is not a real sponsor, nor is it a real company.
-            </p>
-          </div>
         </div>
-      </section>
+        <div class="sponsorLogoBox justify-content-between">
+          <a href="http://kaymckelly.com">
+            <img src="./images/sponsors/ibm.png" alt="IBM" title="IBM" class="sponsorPageLogo">
+          </a>
+        </div>
+        <div class="sponsorLogoBox justify-content-between">
+          <a href="http://kaymckelly.com">
+            <img src="./images/sponsors/auditchain.png" alt="Auditchain" title="Auditchain" class="sponsorPageLogo">
+          </a>
+        </div>
+        <div class="sponsorLogoBox justify-content-between">
+          <a href="http://kaymckelly.com">
+            <img src="./images/sponsors/protocolLabs.png" alt="Protocol Labs" title="Protocol Labs" class="sponsorPageLogo">
+          </a>
+        </div>
+        <div class="sponsorLogoBox justify-content-between">
+          <a href="http://kaymckelly.com">
+            <img src="./images/sponsors/poa.jpg" alt="POA" title="POA" class="sponsorPageLogo">
+          </a>
+        </div>
+        <div class="sponsorLogoBox justify-content-between">
+          <a href="http://kaymckelly.com">
+            <img src="./images/sponsors/iohk.png" alt="IOHK" title="IOHK" class="sponsorPageLogo">
+          </a>
+        </div>
+      </div>
     </main>
     <?php include "includes/footer.php"; ?>
   </body>
