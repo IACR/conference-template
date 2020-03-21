2c06a1714ad1c6cc89b7ee9ac01eccdd75f9c09d
HyungRok Ham <hhr@HHRui-MacBook-Pro.local>
2020-03-21 21:15:25 +0900

    added PC members / updated CFP

--- index.php
+++ index.php
@@ -46,6 +46,14 @@
           </h4>
           <div class="customCardRow row">
             <h6 class="dateTitle col-5 col-md-4">
+              March 21, 2020
+            </h6>
+            <p class="col-7 col-md-8">
+              CFP updated
+            </p>
+          </div>
+          <div class="customCardRow row">
+            <h6 class="dateTitle col-5 col-md-4">
               March 10, 2020
             </h6>
             <p class="col-7 col-md-8">
