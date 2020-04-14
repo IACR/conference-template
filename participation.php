6bba87b97b9578648912f230c1ecda082cc1e721
Kay McKelly <frecklestars@gmail.com>
2020-04-14 10:34:14 -0700

    Adding content to participation page

--- participation.php
+++ participation.php
@@ -19,15 +19,55 @@
       <h2 class="indPageTitle">
         Participation Guidelines
       </h2>
-      <div class="row">
-        <div class="col-12">
-          <p>
-            This information is not yet available. Thank you for your patience.
-          </p>
+      <p>
+        There are many ways you can participate in an IACR virtual conference. Your participation is valuable and we encourage you to add to the discussion. Like a face-to-face conference, virtual conferences are only as good as the interactions that occur there.
+      </p>
 
-          <!-- TODO: this page needs info like links to webinars, how to navigate between sessions, etc -->
+      <div class="row">
+        <div class="col-md-10 mx-md-auto">
+          <article class="customCard">
+            <h4 class="customCardHeader text-center">
+              When does the next session start?
+            </h4>
+            <p class="px-3 pt-2 pb-1 text-center">
+              oooo look, a timer
+            </p>
+          </article>
         </div>
       </div>
+
+      <h3 class="pageSubtitle mt-4">
+        Ways to Participate
+      </h3>
+      <div class="row">
+        <div class="col-md-6 mx-lg-auto col-lg-5 col-xl-4">
+          <h4 class="subSubtitle text-center">
+            Webinar
+          </h4>
+          <p>
+            We will be holding live Q&A panels with authors multiple times daily. At these panels, attendees will be able to ask questions of authors about their work, or to encourage interesting conversation amongst authors. These Q&As will be livestreamed on our YouTube channel and are available for later viewing, should you not wish to watch them live.
+          </p>
+        </div>
+
+        <div class="col-md-6 mx-lg-auto col-lg-5 col-xl-4">
+          <h4 class="subSubtitle text-center">
+            Chat
+          </h4>
+          <p>
+            Much like at a face-to-face conference, we know that the best conversations can happen in the hallways or at coffee breaks. We encourage you to make use of the Slack workspace we have set up to connect with other conference attendees. There are a variety of channels, organized by topic.
+          </p>
+        </div>
+
+        <div class="col-md-6 mx-lg-auto col-lg-5 col-xl-4">
+          <h4 class="subSubtitle text-center">
+            YouTube
+          </h4>
+          <p>
+            If you cannot participate live, you will still have access to all talks on our YouTube channel. The authors have pre-recorded their talks and you can view them at any time. Additionally, there will be copies of the live Q&A sessions available to watch.
+          </p>
+        </div>
+      </div>
+      <!-- TODO: this page needs info like links to webinars, how to navigate between sessions, etc -->
 
     </main>
 
