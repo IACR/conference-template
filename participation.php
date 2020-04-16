16877b7cdc63f519b4bed3525bc0608cfa51ab94
Kay McKelly <frecklestars@gmail.com>
2020-04-16 13:11:17 -0700

    A few more FAQs and some rearranging of content

--- participation.php
+++ participation.php
@@ -20,7 +20,7 @@
         Participation Guidelines
       </h2>
       <div class="row">
-        <div class="col-md-7">
+        <div class="col-md-6">
           <p>
             There are many ways you can participate in an IACR virtual conference. Your participation is valuable and we encourage you to add to the discussion. Like a face-to-face conference, virtual conferences are only as good as the interactions that occur there.
           </p>
@@ -28,60 +28,21 @@
             We recognize that a virtual conference is not a 1:1 substitute for a physical conference. However, we believe there are distinct advantages to having a virtual conference. These include a lack of space constraints, travel is not required, and the ability to attend all talks since they are recorded.
           </p>
         </div>
-        <div class="col-md-5">
-          <p class="alert alert-warning mb-0 mt-md-5">
-            We recommend monitoring <a href="program.php">the conference program</a> to see which sessions are upcoming.
+
+        <div class="col-md-6 mt-md-4">
+          <p class="alert alert-warning">
+            <img src="images/icons/exclamation.svg" class="icon"> All conference attendees must be IACR members. When you participate in Q&amp;A, chat, etc, you must do so in such a way that you are readily identifiable (i.e. by using the name you are known by in professional contexts). The <a href="conduct.php">code of conduct</a> still applies in a virtual setting.
           </p>
         </div>
       </div>
+
 
       <h3 class="pageSubtitle mt-4">
         Schedule Structure
       </h3>
       <p>
-        The conference is organized over 5 days, with 4 hours per day. Sessions consist of live panel discussions (i.e. brief summaries of the papers, which are grouped by topic, followed by a Q&amp;A with the authors), invited talks, and a rump session.
+        The conference is organized over 5 days, with 4 hours per day. Sessions consist of live panel discussions (i.e. brief summaries of the papers, which are grouped by topic, followed by a Q&amp;A with the authors), invited talks, and a rump session. We recommend monitoring <a href="program.php">the conference program</a> to see which sessions are upcoming.
       </p>
-
-      <div class="row">
-        <div class="col-sm-6">
-          <h4 class="subSubtitle text-md-center">
-            Synchronous Participation
-          </h4>
-          <ul>
-            <li>
-              attend a Q&amp;A session via Zoom webinar
-            </li>
-            <li>
-              view a Q&amp;A session on YouTube (streamed live from Zoom webinar)
-            </li>
-            <li>
-              ask a question via Zoom webinar Q&amp;A function
-            </li>
-            <li>
-              host or attend a watch party for a pre-recorded talk
-            </li>
-          </ul>
-        </div>
-
-        <div class="col-sm-6">
-          <h4 class="subSubtitle text-md-center">
-            Asynchronous Participation
-          </h4>
-          <ul>
-            <li>
-              watch a pre-recorded talk on YouTube
-            </li>
-            <li>
-              watch a Q&amp;A session on YouTube (these continue to be available after they are streamed live)
-            </li>
-            <li>
-              participate in chat via Slack
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <!-- TODO: how to participate in session: can view p.d live on zoom/yt or asynch on yt. Q&amp;A primarily live in zoom but will try to monitor the corresponding slack channel. will need to be linked to from program (use id? separate page?) -->
 
       <h3 class="pageSubtitle mt-4">
         Ways to Participate
@@ -174,8 +135,34 @@
         We offer entrance to our webinars for both attendees with Zoom accounts and those without. When you log in via the browser client, Zoom will ask for an email. This does not have to be a legitimate email address. However, if you plan to use the desktop client, you will need a Zoom account.
       </p>
 
-      <p class="alert alert-danger">
-        All conference attendees must be IACR members. When you participate in Q&amp;A, chat, etc, you must do so in such a way that you are readily identifiable (i.e. by using the name you are known by in professional contexts). The <a href="conduct.php">code of conduct</a> still applies in a virtual setting.
+      <h3 id="faq" class="pageSubtitle mt-4">
+        Frequently Asked Questions
+      </h3>
+      <h4 class="subSubtitle">
+        How do I ask a question during the live Q&amp;A sessions?
+      </h4>
+      <p>
+        We strongly encourage you to use the Q&amp;A function in Zoom webinar, but we will also make an effort to monitor the corresponding Slack channel.
+      </p>
+
+      <h4 class="subSubtitle">
+        The session times are not convenient for me. What can I do?
+      </h4>
+      <p>
+        We have expended considerable effort to balance time zone difference concerns. Unfortunately, due to the wide variety of time zones IACR members live in, we cannot accommodate everyone. We strongly encourage those who cannot attend live events to still participate in the conference asynchronously.
+      </p>
+      <p>
+        Live Q&amp;A sessions and invited talks are streamed to YouTube and available for viewing after the fact. You can also watch any of the pre-recorded talks on YouTube, or participate in chat via Slack. If you don't want to watch the talks alone, you could host a watch party for others in your time zone.
+      </p>
+
+      <h4 class="subSubtitle">
+        Zoom/Slack/YouTube have security and privacy issues. Does this mean the IACR endorses these technologies?
+      </h4>
+      <p>
+        No. There was no perfect solution available, and we had a very quick turnaround time, so we opted for commercial options that had a high degree of reliability while offering the best user experience. The IACR acknowledges the security and privacy problems of each of these technologies, and our use of them is not to be considered an endorsement.
+      </p>
+      <p>
+        For future IACR virtual events, we hope to be able to host chat and webinar services on our own servers.
       </p>
 
     </main>
