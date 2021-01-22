6538ea4578ae3b87f08b4b0d28ca4d23d20e61a5
Kay McKelly <kayasmckelly@gmail.com>
2021-01-22 14:51:28 -0800

    Updated registration page

--- registration.php
+++ registration.php
@@ -11,6 +11,7 @@
       <?php echo $META['shortName'];?> Registration
     </title>
   </head>
+
   <body>
     <?php require "includes/nav.php"; ?>
 
@@ -23,13 +24,13 @@
         This information is not yet available. Thank you for your patience.
       </p>
 
-      <!--
-      <p class="editMe">
-         Below is sample information, derived from CRYPTO 2016.
-      </p>
 
-      <div class="row mt-3">
-        <section class="col-12" id="regTable">
+      <!-- <p class="editMe">
+         Below is sample information, derived from CRYPTO 2016.
+      </p> -->
+
+      <div class="row my-4">
+        <section class="col-11 mx-auto" id="regTable">
           <div class="row" id="regTopRow">
             <div class="col-6">
               <p class="regTitle">
@@ -52,17 +53,16 @@
             <div class="col-6">
               <p class="regBodyText">
                 Registration Fee + IACR dues
-                (NOT attending CHES)
               </p>
             </div>
             <div class="col-3">
               <p class="priceText">
-                TBD
+                $50
               </p>
             </div>
             <div class="col-3">
               <p class="priceText">
-                TBD
+                $25
               </p>
             </div>
           </div>
@@ -70,64 +70,33 @@
           <div class="row">
             <div class="col-6">
               <p class="regBodyText">
-                Registration Fee + IACR dues (attending CHES)
+                Attended prior <?php echo date("Y");?> IACR conferences
               </p>
             </div>
             <div class="col-3">
               <p class="priceText">
-                TBD
+                $0
               </p>
             </div>
             <div class="col-3">
               <p class="priceText">
-                TBD
-              </p>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-6">
-              <p class="regBodyText">
-                Attended prior 2017 IACR conferences or workshops (NOT attending CHES)
-              </p>
-            </div>
-            <div class="col-3">
-              <p class="priceText">
-                TBD
-              </p>
-            </div>
-            <div class="col-3">
-              <p class="priceText">
-                TBD
-              </p>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-6">
-              <p class="regBodyText">
-                Attended prior 2017 IACR conferences or workshops (attending CHES)
-              </p>
-            </div>
-            <div class="col-3">
-              <p class="priceText">
-                TBD
-              </p>
-            </div>
-            <div class="col-3">
-              <p class="priceText">
-                TBD
+                $0
               </p>
             </div>
           </div>
         </section>
       </div>
 
-      <div class="row mt-4">
+      <p class="alert customAlert-warm">
+        <img src="images/icons/exclamation.svg" class="icon mb-1 mr-1" /> People who register for the conference agree to abide by the
+        <a href="conduct.php">IACR code of conduct</a>.
+      </p>
+
+      <div class="row my-3">
         <section class="col-md-6">
           <h3 class="pageSubtitle">What the Conference Fee Includes</h3>
           <p>
-            The conference registration fee includes participation in the technical program, all social functions as well as an electronic copy of the conference proceedings. Accompanying guests can also be registered for participation in the conference social functions. Guests who only want to attend one or two events can register onsite.
+            The conference registration fee includes participation in the technical program, all social functions, and an electronic copy of the conference proceedings. Accompanying guests can also be registered for participation in the conference social functions. Guests who only want to attend one or two events can register onsite.
           </p>
           <p>
             Two conferences are located in <?php echo $META['location'];?> on <?php echo $META['dates'];?>. If you are interested in attending both <?php echo $META['shortName'];?> and CHES, there is a $50 joint registration discount ($25 off each conference) for regular registrations. Student joint registrations are discounted by $100 ($50 off each conference).
@@ -136,7 +105,14 @@
         <section class="col-md-6">
           <h3 class="pageSubtitle">An Important Note for Students</h3>
           <p>
-            If you are a student presenting at <?php echo $META['shortName'];?>, your registration fee will be covered by the IACR Fund for Student Presenters; you do not need to pay for registration. Please register for the conference but do not submit any payment. After registering, send an email to the <a href="mailto:doNotEmailMe@fakeDomain.com">General Chair</a>, explaining that you are a student presenter and have already registered. More information is available <a href="./stipends.php">here</a>.
+            If you are a student presenting at <?php echo $META['shortName'];?>,
+            your registration fee will be covered by the Cryptography Research
+            Fund for Student Presenters; you do not need to pay for registration.
+            Please register for the conference but do not submit any payment.
+            After registering, please contact the General Chair, explaining
+            that you are a student presenter and have already registered. More
+            information is available on the <a href="./stipends.php">stipends
+            page</a>.
           </p>
         </section>
       </div>
@@ -146,7 +122,7 @@
           <h5>
             <strong>Registration is not currently available. This button will become active when registration opens.</strong>
           </h5>
-          <a href="https://secure.iacr.org/conferences/tcc2017b/register/" class="btn btn-lg customBtn-warm btn-block regBtn disabled" role="button">Register now!</a>
+          <a href="#" class="btn btn-lg customBtn-warm btn-block disabled" role="button">Register now!</a>
         </aside>
       </div>
 
@@ -191,7 +167,7 @@
           </p>
         </section>
       </div>
-      -->
+
     </main>
 
     <?php include "includes/footer.php"; ?>
