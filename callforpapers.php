0ca96910b2bd0884480d61cc5372588c92232282
Kay McKelly <kayasmckelly@gmail.com>
2022-06-09 17:30:59 -0700

    Better way to do PC & general chairs

--- callforpapers.php
+++ callforpapers.php
@@ -152,9 +152,97 @@
         </script>
       </div>
 
-      <!-- To add/edit contact information, edit the file linked below (includes/chairs.php).
-      BE AWARE that this will change this information on the contact.php page as well! -->
-      <?php require "includes/chairs.php"; ?>
+      <!-- To add/edit contact information, please edit names and affiliations in
+      json/metadata.json -->
+      <h3 class="pageSubtitle text-center mt-4">
+        Program Co-Chairs
+      </h3>
+
+      <!-- NOTE: if you only have one program chair, please uncomment the following div,
+      then remove or comment out the div below containing the PHP  -->
+      <!-- <div class="row mt-3 mt-md-4">
+        <aside class="col-12 text-center mb-2">
+          <h4 class="subSubtitle">
+            <?php echo $META['PC_CHAIRS'][0]['name'] ?>
+          </h4>
+          <p class="text-center">
+            <?php echo $META['PC_CHAIRS'][0]['affiliation'] ?>
+          </p>
+        </aside>
+      </div> -->
+
+      <div class="row mt-3 mt-md-4">
+        <?php
+          foreach ($META['PC_CHAIRS'] as $person) {
+            $name = $person['name'];
+            $affiliation = $person['affiliation'];
+            echo <<< EOR
+            <aside class="col-12 col-sm-6 text-center mb-2">
+              <h4 class="subSubtitle">
+                $name
+              </h4>
+              <p class="text-center">
+                $affiliation
+              </p>
+            </aside>
+            EOR;
+          }
+        ?>
+      </div>
+
+      <div class="row">
+        <div class="col-12 text-center mb-5">
+          <a href="mailto:doNotEmailMe@fakeDomain.com">
+            <img src="images/icons/email.svg" class="icon" />
+          </a> doNotEmailMe@fakeDomain.com
+        </div>
+      </div>
+
+      <!-- To add/edit contact information, please edit names and affiliations in
+      json/metadata.json -->
+      <h3 class="pageSubtitle text-center mt-4">
+        General Co-Chairs
+      </h3>
+
+      <!-- NOTE: if you only have one general chair, please uncomment the following div,
+      then remove or comment out the div below containing the PHP -->
+      <!-- <div class="row mt-3 mt-md-4">
+        <aside class="col-12 text-center mb-2">
+          <h4 class="subSubtitle">
+            <?php echo $META['GENERAL_CHAIRS'][0]['name'] ?>
+          </h4>
+          <p class="text-center">
+            <?php echo $META['GENERAL_CHAIRS'][0]['affiliation'] ?>
+          </p>
+        </aside>
+      </div> -->
+
+      <div class="row mt-3 mt-md-4">
+        <?php
+          foreach ($META['GENERAL_CHAIRS'] as $person) {
+            $name = $person['name'];
+            $affiliation = $person['affiliation'];
+            echo <<< EOR
+            <aside class="col-12 col-sm-6 text-center mb-2">
+              <h4 class="subSubtitle">
+                $name
+              </h4>
+              <p class="text-center">
+                $affiliation
+              </p>
+            </aside>
+            EOR;
+          }
+        ?>
+      </div>
+
+      <div class="row">
+        <div class="col-12 text-center mb-5">
+          <a href="mailto:reallyDoNotEmailMe@fakeDomain.com">
+            <img src="images/icons/email.svg" class="icon" />
+          </a> reallyDoNotEmailMe@fakeDomain.com
+        </div>
+      </div>
 
     </main>
 
